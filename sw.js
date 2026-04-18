bc68957de63f0d854271bccb55674aa291f59568
vishnuputhur <vishnuputhur@yahoo.com>
2026-04-18 21:38:46 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'vsquar-v1';
+const assets = [
+  './',
+  './index.html',
+  './details.html',
+  './transations.html',
+  'https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore-compat.js'
+];
+
+// Install Service Worker
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
