d4726d2930da349172a8f88699908eba9efa45d9
vishnuputhur <vishnuputhur@yahoo.com>
2026-04-21 14:32:35 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,38 @@
-const CACHE_NAME = 'vsquar-v2';
-const assets = [
+// sw.js - Vsquar PWA
+const CACHE_NAME = 'vsquar-v2'; // Increment version on each update
+const urlsToCache = [
   './',
-  './index.html',
-  './details.html',
-  './transations.html',
-  'https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js',
-  'https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore-compat.js'
+  './index.html',   // or the name of your main HTML file
+  './manifest.json',
+  './icon-192.png'
 ];
 
-// Install Service Worker
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  // Force waiting service worker to become active
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
-// Fetch Assets
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
