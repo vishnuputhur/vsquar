63a6ace2649e7d8460cb40a31b2791e3ab8f4f42
vishnuputhur <vishnuputhur@yahoo.com>
2026-08-24 14:23:19 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Vsquar PWA
-const CACHE_NAME = 'vsquar-v12.4'; // Increment version on each update
+const CACHE_NAME = 'vsquar-v12.5'; // Increment version on each update
 const urlsToCache = [
   './',
   './index.html',   // or the name of your main HTML file
